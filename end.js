c489d59464001a035879c1e114dc6e3f554984a8
VIVEK-PATHAK <105923824+VIVEK-PATHAK@users.noreply.github.com>
2022-05-26 21:09:37 +0530

    Update end.js

--- end.js
+++ end.js
@@ -29,6 +29,6 @@ saveHighScore = e => {
 
     highScores.splice(5)  
     localStorage.setItem('highScores', JSON.stringify(highScores)) 
-    window.location.assign('/')
+    window.location.assign('/indexx.html')
 }
 
